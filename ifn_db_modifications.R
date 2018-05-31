abbf9a7bfcd003bc99703b01712ab83ee6e68060
MalditoBarbudo <victorgrandagarcia@gmail.com>
2018-05-31 14:01:27 +0200

    started Coordinates fixes

--- ifn_db_modifications.R
+++ ifn_db_modifications.R
@@ -14,3 +14,14 @@ oracle_ifn <- DBI::dbConnect(
 )
 
 ## Modifications
+
+# Coordinates ####
+
+# Fix coordinates mismatch between IFN2,3 and IFN4
+# IFN2 and 3 comes in ED50 datum (+init=23031), whereas IFN4 comes in more
+# modern ETRS89 datum (+init=25831). Here we will uniformize the utm coords
+# datum in all IFNs, to the more modern ETRS89 datum, so we need to transform
+# coordinates in sig tables for IFN2 and IFN3
+
+# parcelaifn2_sig
+tbl(oracle_ifn, 'parcelaifn2_sig')
