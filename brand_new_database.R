14769f1d7a0ecc88b7338caad3aca9e9fb83e7b6
MalditoBarbudo <victorgrandagarcia@gmail.com>
2018-11-19 14:40:22 +0100

    added bc tables

--- brand_new_database.R
+++ brand_new_database.R
@@ -1517,6 +1517,145 @@ tbl(access4_db,'ResultatEspecie_IFN4_CREAF_OLAP') %>%
     under_bark_volume_dead = vscmorts
   ) -> GENUS_NFI_4_RESULTS
 
+## Results at broadleaf-conifer level for each nfi level
+tbl(oracle_db, 'r_plancon_ifn2_creaf') %>%
+  collect() %>%
+  left_join(plot_id_nfi_2, by = c('idparcela' = 'old_idparcela')) %>%
+  select(
+    plot_id, #everything()
+    bc_id = idplanifconif,
+    basal_area = ab,
+    basal_area_dead = abmorts,
+    dbh = dbh,
+    dbh_dead = dbhmorts,
+    density = densitat,
+    density_dead = densitatmorts,
+    order_basal_area = ordreab,
+    order_density = ordredens,
+    basal_area_percentage = percab,
+    density_percentage = percdens,
+    canopy_cover = rc,
+    what_the_hell_is_this = vc,
+    over_bark_volume = vcc,
+    over_bark_volume_dead = vccmorts,
+    what_the_heel_is_this_dead = vcmorts,
+    under_bark_volume = vsc,
+    under_bark_volume_dead = vscmorts
+  ) -> BC_NFI_2_RESULTS
+
+tbl(oracle_db, 'r_plancon_ifn3_creaf') %>%
+  collect() %>%
+  left_join(
+    plot_id_nfi_3, by = c('idparcela' = 'old_idparcela', 'idclasse' = 'old_idclasse_nfi3')
+  ) %>%
+  select(
+    plot_id, #everything()
+    bc_id = idplanifconif,
+    basal_area = ab,
+    basal_area_dead = abmorts,
+    aerial_biomass_total = bat,
+    trunk_bark_biomass = bc,
+    leaf_biomass = bh,
+    trunk_wood_biomass = bm,
+    branch_wo_leaves_biomass = br,
+    aerial_carbon_total = cat,
+    trunk_bark_carbon = cc,
+    accumulated_aerial_co2 = cca,
+    leaf_carbon = ch,
+    trunk_wood_carbon = cm,
+    branch_wo_leaves_carbon = cr,
+    dbh = dbh,
+    dbh_dead = dbhmorts,
+    density = densitat,
+    density_dead = densitatmorts,
+    order_basal_area = ordreab,
+    order_density = ordredens,
+    basal_area_percentage = percab,
+    density_percentage = percdens,
+    canopy_cover = rc,
+    over_bark_volume = vcc,
+    over_bark_volume_dead = vccmorts,
+    under_bark_volume = vsc,
+    under_bark_volume_dead = vscmorts
+  ) -> BC_NFI_3_RESULTS
+
+tbl(access4_db,'ResultatEspecie_IFN4_CREAF_OLAP') %>%
+  # join the tesaurus with the specie-genus relation
+  left_join({
+    tbl(access4_db, 'TesaureEspecieIFN4') %>%
+      select(Especie, PlanifConif) %>%
+      filter(row_number() %in% c(
+        1:156, 158:327, 330:337, 339:348, 350:352, 354:356, 358:359
+      ))
+  }) %>%
+  select(
+    # ordenar
+    IdParcela, IdClasseIFN4, PlanifConif, everything(),
+    # quitar las que no necesitamos o tenemos que recalcular
+    -Especie, -OrdreDens, -OrdreAB, -DBH, -DBHMorts
+  ) %>%
+  # group by plot, class and genus
+  group_by(IdParcela, IdClasseIFN4, PlanifConif) %>%
+  # summarise all vars, by sum them
+  summarise_all(funs(sum(., na.rm = TRUE))) %>%
+  # create the variables needed, DBH and orders.
+  # DBH = sqrt((AB/Densitat)/pi)*2*100
+  # Ordre* = order in the specified var, but reversed (desc)
+  mutate(
+    DBH = if_else(
+      Densitat < 1, 0, sqrt((AB/Densitat)/pi) * 200
+    ),
+    DBHMorts = if_else(
+      DensitatMorts < 1, 0, sqrt((ABMorts/DensitatMorts)/pi) * 200
+    ),
+    OrdreDens = min_rank(desc(Densitat)),
+    OrdreAB = min_rank(desc(AB))
+  ) %>%
+  select(
+    IdParcela, IdClasseIFN4, PlanifConif, OrdreDens, OrdreAB, PercDensitat,
+    PercAB, Densitat, DensitatMorts, AB, ABMorts, DBH, DBHMorts,
+    everything()
+  ) %>%
+  collect() %>%
+  {magrittr::set_names(., tolower(names(.)))} %>%
+  rename(
+    idclasse = idclasseifn4,
+    idplanifconif = planifconif,
+    percdens = percdensitat
+  ) %>% 
+  left_join(
+    plot_id_nfi_4, by = c('idparcela' = 'old_idparcela', 'idclasse' = 'old_idclasse_nfi4')
+  ) %>%
+  select(
+    plot_id, #everything()
+    bc_id = idplanifconif,
+    basal_area = ab,
+    basal_area_dead = abmorts,
+    aerial_biomass_total = bat,
+    trunk_bark_biomass = bc,
+    leaf_biomass = bh,
+    trunk_wood_biomass = bm,
+    branch_wo_leaves_biomass = br,
+    aerial_carbon_total = cat,
+    trunk_bark_carbon = cc,
+    accumulated_aerial_co2 = cca,
+    leaf_carbon = ch,
+    trunk_wood_carbon = cm,
+    branch_wo_leaves_carbon = cr,
+    dbh = dbh,
+    dbh_dead = dbhmorts,
+    density = densitat,
+    density_dead = densitatmorts,
+    order_basal_area = ordreab,
+    order_density = ordredens,
+    basal_area_percentage = percab,
+    density_percentage = percdens,
+    canopy_cover = rc,
+    over_bark_volume = vcc,
+    over_bark_volume_dead = vccmorts,
+    under_bark_volume = vsc,
+    under_bark_volume_dead = vscmorts
+  ) -> BC_NFI_4_RESULTS
 
 
 
