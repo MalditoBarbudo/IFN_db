6d1db9fc108b411ca1319e0aa83c7593b4a488be
MalditoBarbudo <victorgrandagarcia@gmail.com>
2018-11-20 11:02:24 +0100

    recalculated the etp and etr from the miramon maps

--- brand_new_database.R
+++ brand_new_database.R
@@ -556,7 +556,7 @@ tbl(oracle_db, 'parcelaifn3') %>%
               'radiacio' = 'rad', 'temperaturaminima' = 'tmin',
               'temperaturamaxima' = 'tmax', 'temperaturamitjana' = 'tmean',
               'precipitacio' = 'prec',
-              'etr_s_' = 'etr_s', 'etr_p_' = 'etr_p'
+              'etr_s_' = 'etr_s', 'etr_p_' = 'etr_p', 'etp_' = 'etp'
             )
           ))
         } %>%
@@ -664,7 +664,7 @@ tbl(oracle_db, 'parcelaifn2') %>%
               'radiacio' = 'rad', 'temperaturaminima' = 'tmin',
               'temperaturamaxima' = 'tmax', 'temperaturamitjana' = 'tmean',
               'precipitacio' = 'prec',
-              'etr_s_' = 'etr_s', 'etr_p_' = 'etr_p'
+              'etr_s_' = 'etr_s', 'etr_p_' = 'etr_p', 'etp_' = 'etp'
             )
           ))
         } %>%
@@ -960,6 +960,71 @@ ifn2_ifn3_ifn4_plots %>%
   ) -> PLOTS
 
 ## TODO Recalculate ETR and ETP from Miramon maps
+# Miramon maps are in ED50 projection, so we need to convert the coordinates that we
+# have from ETRS89 to ED50, load the raster images in the geotiff format (with the
+# raster::raster function) and extract the values at the coordinates (with the
+# raster::extract function)
+PLOTS %>%
+  select(coords_utm_x_ETRS89, coords_utm_y_ETRS89) %>% 
+  sp::SpatialPoints(sp::CRS('+init=epsg:23031')) -> plots_sppoints_ed50
+
+etp_year_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S.tif')
+etp_jan_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_1.tif')
+etp_feb_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_2.tif')
+etp_mar_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_3.tif')
+etp_apr_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_4.tif')
+etp_may_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_5.tif')
+etp_jun_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_6.tif')
+etp_jul_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_7.tif')
+etp_aug_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_8.tif')
+etp_sep_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_8.tif')
+etp_oct_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_10.tif')
+etp_nov_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_11.tif')
+etp_dec_raster <- raster::raster('data_raw/mmm_geotiffs/ETP_S versión GeoTIF/ETP_S_12.tif')
+etr_year_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10.tif')
+etr_jan_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_1.tif')
+etr_feb_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_2.tif')
+etr_mar_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_3.tif')
+etr_apr_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_4.tif')
+etr_may_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_5.tif')
+etr_jun_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_6.tif')
+etr_jul_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_7.tif')
+etr_aug_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_8.tif')
+etr_sep_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_8.tif')
+etr_oct_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_10.tif')
+etr_nov_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_11.tif')
+etr_dec_raster <- raster::raster('data_raw/mmm_geotiffs/ETR_K10 versión GeoTIF/ETR_K10_12.tif')
+
+PLOTS %<>%
+  select(-starts_with('clim_etp_'), -starts_with('clim_etr_')) %>%
+  mutate(
+    clim_etp_year = raster::extract(etp_year_raster, plots_sppoints_ed50),
+    clim_etp_jan = raster::extract(etp_jan_raster, plots_sppoints_ed50),
+    clim_etp_feb = raster::extract(etp_feb_raster, plots_sppoints_ed50),
+    clim_etp_mar = raster::extract(etp_mar_raster, plots_sppoints_ed50),
+    clim_etp_apr = raster::extract(etp_apr_raster, plots_sppoints_ed50),
+    clim_etp_may = raster::extract(etp_may_raster, plots_sppoints_ed50),
+    clim_etp_jun = raster::extract(etp_jun_raster, plots_sppoints_ed50),
+    clim_etp_jul = raster::extract(etp_jul_raster, plots_sppoints_ed50),
+    clim_etp_aug = raster::extract(etp_aug_raster, plots_sppoints_ed50),
+    clim_etp_sep = raster::extract(etp_sep_raster, plots_sppoints_ed50),
+    clim_etp_oct = raster::extract(etp_oct_raster, plots_sppoints_ed50),
+    clim_etp_nov = raster::extract(etp_nov_raster, plots_sppoints_ed50),
+    clim_etp_dec = raster::extract(etp_dec_raster, plots_sppoints_ed50),
+    clim_etr_year = raster::extract(etr_year_raster, plots_sppoints_ed50),
+    clim_etr_jan = raster::extract(etr_jan_raster, plots_sppoints_ed50),
+    clim_etr_feb = raster::extract(etr_feb_raster, plots_sppoints_ed50),
+    clim_etr_mar = raster::extract(etr_mar_raster, plots_sppoints_ed50),
+    clim_etr_apr = raster::extract(etr_apr_raster, plots_sppoints_ed50),
+    clim_etr_may = raster::extract(etr_may_raster, plots_sppoints_ed50),
+    clim_etr_jun = raster::extract(etr_jun_raster, plots_sppoints_ed50),
+    clim_etr_jul = raster::extract(etr_jul_raster, plots_sppoints_ed50),
+    clim_etr_aug = raster::extract(etr_aug_raster, plots_sppoints_ed50),
+    clim_etr_sep = raster::extract(etr_sep_raster, plots_sppoints_ed50),
+    clim_etr_oct = raster::extract(etr_oct_raster, plots_sppoints_ed50),
+    clim_etr_nov = raster::extract(etr_nov_raster, plots_sppoints_ed50),
+    clim_etr_dec = raster::extract(etr_dec_raster, plots_sppoints_ed50)
+  )
 
 
 #### STEP 7 ####
