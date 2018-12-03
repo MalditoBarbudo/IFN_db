573a70fe1c973e096b2ba02f53aa450a0e5900b0
MalditoBarbudo <victorgrandagarcia@gmail.com>
2018-12-03 16:48:27 +0100

    added plot comparision tables for diameter classes

--- brand_new_database.R
+++ brand_new_database.R
@@ -3075,6 +3075,146 @@ tbl(access4_db, 'Resultat_IFN4_IFN3_CREAF_OLAP') %>%
     dbh_dead = dbh_dead / years_diff
   ) -> PLOT_COMP_NFI3_NFI4_RESULTS
 
+# diameter classes
+tbl(oracle_db, 'r_cd_ifn3_ifn2_creaf') %>%
+  collect() %>%
+  # we remove the a4c and a6c plots as they have no data for ifn2 comparable at tree level
+  # (the origin of the comparisions)
+  filter(!idclasse %in% c('A4C', 'A6C')) %>%
+  left_join(
+    plot_id_nfi_3 %>% select(old_idparcela, old_idclasse_nfi3, plot_id),
+    by = c('idparcela' = 'old_idparcela', 'idclasse' = 'old_idclasse_nfi3')
+  ) %>%
+  filter(!is.na(plot_id)) %>%
+  select(
+    plot_id,
+    diamclass_id = idcd,
+    density_rem = densitat_d,
+    density_inc = densitat_i,
+    density_dead = densitat_m,
+    density_balance = densitat_balanç,
+    density_growth = densitat_creixement,
+    basal_area_rem = ab_d,
+    basal_area_inc = ab_i,
+    basal_area_dead = ab_m,
+    basal_area_balance = ab_balanç,
+    basal_area_growth = ab_creixement,
+    volume_over_bark_rem = vcc_d,
+    volume_over_bark_inc = vcc_i,
+    volume_over_bark_dead = vcc_m,
+    volume_over_bark_balance = vcc_balanç,
+    volume_over_bark_growth = vcc_creixement,
+    volume_under_bark_rem = vsc_d,
+    volume_under_bark_inc = vsc_i,
+    volume_under_bark_dead = vsc_m,
+    volume_under_bark_balance = vsc_balanç,
+    volume_under_bark_growth = vsc_creixement
+    # dbh_rem = dbh_d,
+    # dbh_inc = dbh_i,
+    # dbh_dead = dbh_m,
+    # dbh_balance = dbh_balanç
+  ) %>%
+  # for the _d, _a, _i and _m vars we need to divide by the years between samplings to
+  # express the vars as a rate. We get the years from the dynamic info tables,
+  # substracting the nfi2 start year from nfi3 start year
+  left_join(
+    PLOTS_NFI_2_DYNAMIC_INFO %>% select(plot_id, feat_sampling_year), by = 'plot_id'
+  ) %>%
+  left_join(
+    PLOTS_NFI_3_DYNAMIC_INFO %>% select(plot_id, feat_sampling_year), by = 'plot_id',
+    suffix = c('_nfi2', '_nfi3')
+  ) %>%
+  mutate(
+    years_diff = feat_sampling_year_nfi3 - feat_sampling_year_nfi2,
+    density_rem = density_rem / years_diff,
+    density_inc = density_inc / years_diff,
+    density_dead = density_dead / years_diff,
+    basal_area_rem = basal_area_rem / years_diff,
+    basal_area_inc = basal_area_inc / years_diff,
+    basal_area_dead = basal_area_dead / years_diff,
+    volume_over_bark_rem = volume_over_bark_rem / years_diff,
+    volume_over_bark_inc = volume_over_bark_inc / years_diff,
+    volume_over_bark_dead = volume_over_bark_dead / years_diff,
+    volume_under_bark_rem = volume_under_bark_rem / years_diff,
+    volume_under_bark_inc = volume_under_bark_inc / years_diff,
+    volume_under_bark_dead = volume_under_bark_dead / years_diff
+    # dbh_rem = dbh_rem / years_diff,
+    # dbh_inc = dbh_inc / years_diff,
+    # dbh_dead = dbh_dead / years_diff
+  ) -> PLOT_COMP_NFI2_NFI3_DIAMCLASS_RESULTS
+
+tbl(access4_db, 'ResultatCD_IFN4_IFN3_CREAF_OLAP') %>%
+  collect() %>%
+  {magrittr::set_names(., tolower(names(.)))} %>%
+  filter(idclasseifn4 == 'A1') %>%
+  left_join(
+    plot_id_nfi_4 %>% select(old_idparcela, old_idclasse_nfi4, plot_id),
+    by = c('idparcela' = 'old_idparcela', 'idclasseifn4' = 'old_idclasse_nfi4')
+  ) %>%
+  select(
+    plot_id,
+    diamclass_id = idcd,
+    density_diss = densitat_d,
+    density_inc = densitat_i,
+    density_dead = densitat_m,
+    density_har = densitat_a,
+    density_balance = densitat_balanç,
+    density_growth = densitat_creixement,
+    basal_area_diss = ab_d,
+    basal_area_inc = ab_i,
+    basal_area_dead = ab_m,
+    basal_area_har = ab_a,
+    basal_area_balance = ab_balanç,
+    basal_area_growth = ab_creixement,
+    volume_over_bark_diss = vcc_d,
+    volume_over_bark_inc = vcc_i,
+    volume_over_bark_dead = vcc_m,
+    volume_over_bark_har = vcc_a,
+    volume_over_bark_balance = vcc_balanç,
+    volume_over_bark_growth = vcc_creixement,
+    volume_under_bark_diss = vsc_d,
+    volume_under_bark_inc = vsc_i,
+    volume_under_bark_dead = vsc_m,
+    volume_under_bark_har = vsc_a,
+    volume_under_bark_balance = vsc_balanç,
+    volume_under_bark_growth = vsc_creixement
+  ) %>%
+  mutate(
+    density_rem = density_har + density_diss,
+    basal_area_rem = basal_area_har + basal_area_diss,
+    volume_over_bark_rem = volume_over_bark_har + volume_over_bark_diss,
+    volume_under_bark_rem = volume_under_bark_har + volume_under_bark_diss
+  ) %>%
+  # for the _d, _a, _i and _m vars we need to divide by the years between samplings to
+  # express the vars as a rate. We get the years from the dynamic info tables,
+  # substracting the nfi2 start year from nfi3 start year
+  left_join(
+    PLOTS_NFI_3_DYNAMIC_INFO %>% select(plot_id, feat_sampling_year), by = 'plot_id'
+  ) %>%
+  left_join(
+    PLOTS_NFI_4_DYNAMIC_INFO %>% select(plot_id, feat_sampling_year), by = 'plot_id',
+    suffix = c('_nfi3', '_nfi4')
+  ) %>%
+  mutate(
+    years_diff = feat_sampling_year_nfi4 - feat_sampling_year_nfi3,
+    density_diss = density_diss / years_diff,
+    density_rem = density_rem / years_diff,
+    density_inc = density_inc / years_diff,
+    density_dead = density_dead / years_diff,
+    basal_area_diss = basal_area_diss / years_diff,
+    basal_area_rem = basal_area_rem / years_diff,
+    basal_area_inc = basal_area_inc / years_diff,
+    basal_area_dead = basal_area_dead / years_diff,
+    volume_over_bark_diss = volume_over_bark_diss / years_diff,
+    volume_over_bark_rem = volume_over_bark_rem / years_diff,
+    volume_over_bark_inc = volume_over_bark_inc / years_diff,
+    volume_over_bark_dead = volume_over_bark_dead / years_diff,
+    volume_under_bark_diss = volume_under_bark_diss / years_diff,
+    volume_under_bark_rem = volume_under_bark_rem / years_diff,
+    volume_under_bark_inc = volume_under_bark_inc / years_diff,
+    volume_under_bark_dead = volume_under_bark_dead / years_diff
+  ) -> PLOT_COMP_NFI3_NFI4_DIAMCLASS_RESULTS
+
 #### STEP 9 Functional groups comparision tables ####
 
 ## species
