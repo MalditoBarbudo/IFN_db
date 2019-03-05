1b0328d327ef806d82a78bd8e0708e33f85eae83
MalditoBarbudo <victorgrandagarcia@gmail.com>
2019-03-05 09:34:59 +0100

    modified migrating script

--- xx_migrating_db.R
+++ xx_migrating_db.R
@@ -1,17 +1,24 @@
-# script to migrating the db to the server
-
-## local
-"pg_dump -C -c -W -U ifn tururu -F p > tururu_dump.sql"
+# dump the data base
+"pg_dump -U ifn tururu > tururu_dump.sql"
+# scp the database
 "scp tururu_dump.sql vgranda@158.109.46.23:~/"
 
-## remote
-"psql tururu < tururu_dump.sql"
+# go to the server and drop the database
+"DROP DATABASE tururu;"
+
+# create an empty database
+"CREATE DATABASE tururu;"
+
+# load the dump in the new database
+"psql -d tururu -f tururu_dump.sql"
 
 # giving access to guest ####
-"GRANT CONNECT ON DATABASE tururu TO guest;
+"
+GRANT CONNECT ON DATABASE tururu TO guest;
 GRANT USAGE ON SCHEMA public TO guest;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO guest;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO guest;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT SELECT ON TABLES TO guest;"
+GRANT SELECT ON TABLES TO guest;
+"
